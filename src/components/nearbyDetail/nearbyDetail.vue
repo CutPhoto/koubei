f3acd945b6edee7596df8736cf272e44b7a2ed65
Cazz93 <czz_ahh_iiun@163.com>
2018-06-28 17:45:39 +0800

    17:45

--- src/components/nearbyDetail/nearbyDetail.vue
+++ src/components/nearbyDetail/nearbyDetail.vue
@@ -1,3 +1,28 @@
-<template></template>
+<template>
+	<div id="nearbyDetail">
+		<div class="detailImg">
+			<img src="../../../static/img/detailImg0.png">
+		</div>
+		<div>
+			<h3>稻家香|鲜鲍猪骨节瓜窝套餐</h3>
+			<p>48元<span>已售套餐<i>47份</i></span></p>
+			<p><span><i></i>随时退</span><span><i></i>过期退</span></p>	
+		</div>	
+		<div>
+			<p>适用门店</p>
+			<p>稻香酒家</p>
+			<p>宝华路133号恒宝广场3楼xxx铺</p>
+		</div>
+		<div>
+			<h3>详细内容</h3>
+			<ul>
+				<li>鲜鲍猪骨节瓜窝（1份）<span></span></li>
+				<li>25头鲜鲍鱼子（10只）</li>
+				<p>查看图文详情&gt;</p>	
+			</ul>
+		</div>	
+		<div>更多本店宝贝</div>	
+	</div>
+</template>
 <script type="text/javascript"></script>
 <style type="text/css"></style>
