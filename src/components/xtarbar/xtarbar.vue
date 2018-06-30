dbefede65b4886ae5d1e207e15892c5f8d97708f
hejie0803 <592649625@qq.com>
2018-06-30 16:19:29 +0800

    chiji1

--- src/components/xtarbar/xtarbar.vue
+++ src/components/xtarbar/xtarbar.vue
@@ -2,31 +2,29 @@
 	<div class="weui-tabbar" :style="{
     height:'53px'
     }">
-<<<<<<< HEAD
-                <a href="/home" class="weui-tabbar__item weui-bar__item_on">
-=======
-                <a href="../home" class="weui-tabbar__item weui-bar__item_on">
->>>>>>> 985677b5f6693de44030afb4ceb9cc9f991686fd
-                    <img src="../../assets/bottom1.jpg" alt="" class="weui-tabbar__icon">
-                    <p class="weui-tabbar__label" style="color:orange">首页</p>
-                </a>
-                <a href="/nearby" class="weui-tabbar__item">
-                    <img src="../../assets/bottom2.jpg" alt="" class="weui-tabbar__icon">
-                    <p class="weui-tabbar__label">附近</p>
-                </a>
-                <a href="/liftCircle" class="weui-tabbar__item">
-                    <img src="../../assets/bottom3.jpg" alt="" class="weui-tabbar__icon">
-                    <p class="weui-tabbar__label">生活圈</p>
-                </a>
-                <a href="/myhome" class="weui-tabbar__item">
-                    <img src="../../assets/bottom4.jpg" alt="" class="weui-tabbar__icon">
-                    <p class="weui-tabbar__label">我的</p>
-                </a>
-            </div>
+
+		<a href="../home" class="weui-tabbar__item weui-bar__item_on">
+			<img src="../../assets/bottom1.jpg" alt="" class="weui-tabbar__icon">
+			<p class="weui-tabbar__label" style="color:orange">首页</p>
+		</a>
+		<a href="/nearby" class="weui-tabbar__item">
+			<img src="../../assets/bottom2.jpg" alt="" class="weui-tabbar__icon">
+			<p class="weui-tabbar__label">附近</p>
+		</a>
+		<a href="/liftCircle" class="weui-tabbar__item">
+			<img src="../../assets/bottom3.jpg" alt="" class="weui-tabbar__icon">
+			<p class="weui-tabbar__label">生活圈</p>
+		</a>
+		<a href="/myhome" class="weui-tabbar__item">
+			<img src="../../assets/bottom4.jpg" alt="" class="weui-tabbar__icon">
+			<p class="weui-tabbar__label">我的</p>
+		</a>
+	</div>
 </template>
 
 <script>
 </script>
 
 <style>
+
 </style>
